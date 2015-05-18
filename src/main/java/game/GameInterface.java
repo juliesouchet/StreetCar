9b8848cfeed69bfe1f81b702cde980e46e2e788c
Valysaar <valpruliere@gmail.com>
2015-05-18 22:15:26 +0000

    interface player and interface game

--- src/main/java/game/GameInterface.java
+++ src/main/java/game/GameInterface.java
@@ -1,8 +1,10 @@
 package main.java.game;
 
+import java.awt.Point;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import main.java.data.Tile;
 import main.java.player.PlayerInterface;
 
 
@@ -11,6 +13,16 @@ import main.java.player.PlayerInterface;
 
 public interface GameInterface extends Remote
 {
-	public void		onJoinRequest	(PlayerInterface player)			throws RemoteException, ExceptionFullParty, ExceptionUsedPlayerName, ExceptionUsedPlayerColor;
-	public boolean	quitGame		(String gameName, String playerName)throws RemoteException;
+	public void		onJoinRequest		(String playerName, PlayerInterface player)				throws RemoteException, ExceptionFullParty, ExceptionUsedPlayerName, ExceptionUsedPlayerColor;
+	public boolean	quitGame			(String playerName, String gameName)					throws RemoteException;
+	public void		hostStartGame		(String playerName)										throws RemoteException;
+	public void		undoAttempt			(String playerName)										throws RemoteException;
+	public void		undoTurn			(String playerName)										throws RemoteException;
+	public void		placeTile			(String playerName, Tile t, Point position)				throws RemoteException;
+	public void		moveTram			(String playerName, Point dest)							throws RemoteException;
+	public void		pickTileFromBox		(String playerName)										throws RemoteException;
+	public void		pickTileFromPlayer	(String playerName, String chosenPlayer, Tile car)		throws RemoteException;
+	public void		replaceTwoTiles		(String playerName, Tile t1, Tile t2, Point p1, Point p2)throws RemoteException;
+	public void		startMaidenTravel	(String playerName, Point terminus)						throws RemoteException;
+	public void		Validate			(String playerName)										throws RemoteException;
 }
