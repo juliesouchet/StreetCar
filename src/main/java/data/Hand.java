318ca7094161544bf40bac22e1b77efa8f101eac
simbadSid <sidlakhdar.riyane@gmail.com>
2015-05-21 11:53:02 +0200

    classe abstraite Player automaton: c'est l'IA Une premiere version d'un adversaire automatique est implémenté: dumbest, il ne fait que poser des tuiles au petit bonheur la chance.
    On a ajouté la classe hand qui est les cartes disponibles pour le joueur

--- src/main/java/data/Hand.java
+++ src/main/java/data/Hand.java
@@ -1,9 +1,32 @@
 package main.java.data;
 
+import java.util.LinkedList;
+
 
 
 
 public class Hand
 {
-
+	private LinkedList<Tile> playerTiles;	// Move Attributes
+	private int size;
+	
+	/**
+	 *  @param Tiles
+	 *  basicsTiles les tuiles standard en début de jeu
+	 */
+	public Hand( LinkedList<Tile> basicsTiles){
+		this.size = basicsTiles.size();
+		this.playerTiles = basicsTiles;
+	}
+	
+	public int size(){
+		return this.size;
+	}
+	
+	public Tile get(int index){
+		return this.playerTiles.get(index);
+	}
+	
+	
+	
 }
