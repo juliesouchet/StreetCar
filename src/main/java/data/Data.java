4d4bc14ff9952c75c1b3893f3d17da0f9144f333
Valysaar <valpruliere@gmail.com>
2015-05-29 17:17:50 +0000

    4 integration ihm

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -212,6 +212,7 @@ public class Data implements Serializable
 // --------------------------------------------
 	public Tile[][]				getBoard()										{return new Copier<Tile>().copyMatrix(this.board);}
 	public String				getHost()										{return new String(this.host);}
+	public String[]				getPlayerOrder()								{return (new Copier<String>()).copyTab(playerOrder);}
 	public int					getWidth()										{return this.board.length;}
 	public int					getHeight()										{return this.board[0].length;}
 	public int					getNbrPlayer()									{return this.playerInfoList.size();}
