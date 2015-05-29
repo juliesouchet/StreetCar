b26d75282287b971a94c3ab59a6e2a88fb7c51f8
UlysseCoutaud <luly_30@hotmail.fr>
2015-05-29 18:31:55 +0200

    suite des tests sur DecisionNode (evaluation des coups) creation de la classe DecisionTable

--- src/main/java/automaton/test/testDecisionNode.java
+++ src/main/java/automaton/test/testDecisionNode.java
@@ -14,37 +14,57 @@ public class testDecisionNode {
 	/**
 	 * @param args
 	 */
-	
-	
+
+
 	public static void main(String[] args) {
 		TraceDebugAutomate.decisionNodeTrace=true;
 		DecisionNode monNoeudDeDecision = null;
-		try {
-			monNoeudDeDecision = new DecisionNode(3, 0, "root");
-		} catch (ExceptionUnknownNodeType e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//System.out.println(monNoeudDeDecision.toString());
+		int taille_de_case = 5000;
+		int nb_de_case = 30000000;
+		long sommeTps = 0;
+		/*
+		 * Notes:
+		 * decisionNode Trace:moyenne pour 1case de 5000actions= 7.09E-7s 
+		 * decisionNode Trace:topdecisionNode Trace:Temps pour 30000000 new decisionNode de taille 5000 = 21.470737712000002
+		 */
 		
+		TraceDebugAutomate.debugDecisionNodeTrace("top");
+		long tmps3 = System.nanoTime();
+		for(int i=0;i<5000;i++){
+			try {
+				long tmps1 = System.nanoTime();
+				monNoeudDeDecision = new DecisionNode(taille_de_case, 0, "root");
+				long tmps2 = System.nanoTime();
+				//TraceDebugAutomate.debugDecisionNodeTrace("Temps pour new decisionNode de taille "+taille_de_case+" = "+ (tmps2-tmps1)*Math.pow(10.0, -9)+"s \n");
+				sommeTps+=(tmps2-tmps1);
+			} catch (ExceptionUnknownNodeType e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		long tmps4 = System.nanoTime();
+		TraceDebugAutomate.debugDecisionNodeTrace("Temps pour "+nb_de_case+" new decisionNode de taille "+taille_de_case+" = "+ (tmps4-tmps3)*Math.pow(10.0, -9)+"s \n");
+		TraceDebugAutomate.debugDecisionNodeTrace("moyenne pour 1case de "+taille_de_case+"actions= "+ (sommeTps/30000000)*Math.pow(10.0, -9)+"s \n");
+		//System.out.println(monNoeudDeDecision.toString());
+
 		Tile maTile = Tile.parseTile("Tile_FFFFZZ060123");
 		Action monAction = Action.newBuildSimpleAction(new Point(5,4), maTile);
-		
+
 		CoupleActionIndex monCoupleActionIndex1 = monNoeudDeDecision.new CoupleActionIndex(monAction, 0);
 		CoupleActionIndex monCoupleActionIndex2 = monNoeudDeDecision.new CoupleActionIndex(monAction, 1);
 
 		monNoeudDeDecision.setCoupleActionIndex(0, monCoupleActionIndex1);
-		
+
 		monNoeudDeDecision.setCoupleActionIndex(1, monCoupleActionIndex2);
-		
 
-		System.out.println("(1)"+monNoeudDeDecision.toString());
-		
+
+		//System.out.println("(1)"+monNoeudDeDecision.toString());
+
 		monCoupleActionIndex1.setIndex(8);
-		System.out.println("(2)"+monNoeudDeDecision.toString());
-		
+		//System.out.println("(2)"+monNoeudDeDecision.toString());
 
-		
+
+
 	}
 
 }
