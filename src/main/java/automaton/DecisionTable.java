1aab74be165723242064105caefad355dd142d01
UlysseCoutaud <luly_30@hotmail.fr>
2015-06-01 09:52:36 +0200

    correction des fonctions getBestAction et getWorstAction

--- src/main/java/automaton/DecisionTable.java
+++ src/main/java/automaton/DecisionTable.java
@@ -13,8 +13,8 @@ public class DecisionTable {
 	private DecisionNode[] NodeTable;
 	private boolean[] freeSlots;
 	private int size;
-	
-	
+
+
 	/**
 	 * Met la valeur de la taille de la table a size
 	 * @param size
@@ -22,7 +22,7 @@ public class DecisionTable {
 	public void setSize(int size){
 		this.size=size;
 	}
-	
+
 	/**
 	 * renvoi la taille de la table
 	 * @return
@@ -47,7 +47,7 @@ public class DecisionTable {
 		this.NodeTable[index].copy(node);
 		this.freeSlots[index]=false;
 	}
-	
+
 	/**
 	 * initie (alloue) le noeud numero index a node
 	 * @param index
@@ -57,42 +57,91 @@ public class DecisionTable {
 		this.NodeTable[index]=node;
 		this.freeSlots[index]=true;
 	}
-	
-	
+
+
 	/**
-	 * retourne l'action possible ayant la configuration correspondante la plus avantageuse
+	 * PRECONDITION: Le noeud doit avoir 1 action possible (retourne -1 sinon)
+	 * retourne le numero de l'action possible ayant la configuration correspondante la plus avantageuse
 	 * @param index
 	 * @return
 	 */
 	public int getBestActionIndex(int index){
-		int indexOfBestAction=0;
-		for(int i=0; i<this.getDecisionNode(index).getNumberOfPossiblesActions();i++){
-			if(this.getDecisionNode(indexOfBestAction).getQuality()<this.getDecisionNode(i).getQuality()){
-				indexOfBestAction=i;
+		int indexBestActionInTable=-1;
+		int indexBestActionInNode=-1;
+		int indexCourantInTable;
+		int indexCourantInNode;
+		double bestValue=-1;
+		double currentValue;
+		DecisionNode decisionNode = this.getDecisionNode(index);
+
+		for (indexCourantInNode=0;indexCourantInNode<decisionNode.getNumberOfPossiblesActions();indexCourantInNode++){
+			indexCourantInTable = decisionNode.getCoupleActionIndex(indexCourantInNode).getIndex();
+			if (indexCourantInTable!=0){
+				currentValue = this.getDecisionNode(indexCourantInTable).getQuality();
+				if( currentValue>=bestValue){
+					indexBestActionInNode = indexCourantInNode;
+					indexBestActionInTable = decisionNode.getCoupleActionIndex(indexBestActionInNode).getIndex();
+					bestValue = this.getDecisionNode(indexBestActionInTable).getQuality();
+				}
 			}
 		}
-		return indexOfBestAction;
-	}
+		return indexBestActionInNode;
+	}		
+
+
+
+
+		//		DecisionNode decisionNode = this.getDecisionNode(index);
+		//		int n=0;
+		//		
+		//		CoupleActionIndex currentCouple, bestCouple;
+		//		int currentValue, bestValue;
+		//		decisionNode.getCoupleActionIndex(0).getIndex();
+		//		for(int i=0; i<decisionNode.getNumberOfPossiblesActions();i++){
+		//			currentCouple = decisionNode.getCoupleActionIndex(i);
+		//			currentValue = currentCouple.getIndex();
+		//			valueBestAction = this.getDecisionNode(index).getCoupleActionIndex(indexOfBestAction).getIndex();
+		//			if(this.getDecisionNode(index).getCoupleActionIndex(indexOfBestAction).getIndex()<this.getDecisionNode(index).getCoupleActionIndex(i)){
+		//				indexOfBestAction=i;
+		//			}
+		//		}
+		//		return n;
+
 	/**
-	 * retourne l'action possible ayant la configuration correspondante la plus desantageuse
+	 * PRECONDITION: Le noeud doit avoir 1 action possible (retourne -1 sinon)
+	 * retourne le numero de l'action possible ayant la configuration correspondante la plus avantageuse
 	 * @param index
 	 * @return
 	 */
 	public int getWorstActionIndex(int index){
-		int indexOfWorstAction=0;
-		for(int i=0; i<this.getDecisionNode(index).getNumberOfPossiblesActions();i++){
-			if(this.getDecisionNode(indexOfWorstAction).getQuality()>this.getDecisionNode(i).getQuality()){
-				indexOfWorstAction=i;
+		int indexWorstActionInTable=-1;
+		int indexWorstActionInNode=-1;
+		int indexCourantInTable;
+		int indexCourantInNode;
+		double worstValue=+101.0;
+		double currentValue;
+		DecisionNode decisionNode = this.getDecisionNode(index);
+
+		for (indexCourantInNode=0;indexCourantInNode<decisionNode.getNumberOfPossiblesActions();indexCourantInNode++){
+			indexCourantInTable = decisionNode.getCoupleActionIndex(indexCourantInNode).getIndex();
+			if (indexCourantInTable!=0){
+				currentValue = this.getDecisionNode(indexCourantInTable).getQuality();
+				if( currentValue<=worstValue){
+					indexWorstActionInNode = indexCourantInNode;
+					indexWorstActionInTable = decisionNode.getCoupleActionIndex(indexWorstActionInNode).getIndex();
+					worstValue = this.getDecisionNode(indexWorstActionInTable).getQuality();
+				}
 			}
 		}
-		return indexOfWorstAction;
+		return indexWorstActionInNode;
 	}	
-	
+
 	@Override
 	public String toString(){
 		String result="Table (size="+this.getSize()+"):\n";
 		TraceDebugAutomate.debugDecisionTableTrace("toString: Begin ");
 		for(int i=0;i<this.getSize();i++){
+			result+="["+i+"]";
 			TraceDebugAutomate.debugDecisionTableTrace("\n toString: ("+i+")");
 			if(!this.freeSlots[i]){
 				for(int j=0;j<this.getDecisionNode(i).getDepth();j++){
@@ -104,7 +153,7 @@ public class DecisionTable {
 		return result;
 	}
 
-	
+
 	/**
 	 * Creer une table de decision de taille size
 	 * Pour etre sur que l'allocation mémoire est faire en amont, je la rempli de decisionsNodes non représentatifs
@@ -122,6 +171,6 @@ public class DecisionTable {
 			this.freeSlots[i]=true;
 		}
 	}
-	
-	
+
+
 }
