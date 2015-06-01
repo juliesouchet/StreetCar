2f868b3e9f2a2a60ccd5fe8d51abf0ecf9d87f67
UlysseCoutaud <luly_30@hotmail.fr>
2015-06-02 01:40:45 +0200

    Implémentation d'une partie des tests unitaires pour action

--- src/test/java/game/TestUnitaireAction.java
+++ src/test/java/game/TestUnitaireAction.java
@@ -1,8 +1,13 @@
 package test.java.game;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.awt.Point;
+
 import main.java.data.Action;
+import main.java.data.Tile;
 
 import org.junit.Test;
 
@@ -23,27 +28,62 @@ public class TestUnitaireAction {
 
 	@Test
 	public void testNewMoveAction() {
-		Action monAction = Action.newMoveAction();
-		assertTrue(monAction.action==Action.START_TRIP_NEXT_TURN);	}
+		Point[] chemin = new Point[10];
+		for(int i=0; i<chemin.length;i++){
+			chemin[i] = new Point(i,i);
+		}
+		Action monAction = Action.newMoveAction(chemin);
+		assertTrue(monAction.action==Action.MOVE);
+		assertTrue(chemin[0].equals(new Point(0,0)));
+		assertTrue(chemin[9].equals(new Point(9,9)));
+	}
 
 	@Test
 	public void testNewBuildSimpleActionPointTile() {
-		fail("Not yet implemented");
+		Action monAction = Action.newBuildSimpleAction(new Point(1,1), Tile.parseTile("Tile_FFFFZZ060123"));
+		assertTrue(monAction.positionTile1.equals(new Point(1,1)));
+		assertTrue(monAction.tile1.equals(Tile.parseTile("Tile_FFFFZZ060123")));
 	}
 
 	@Test
 	public void testNewBuildSimpleActionIntIntTile() {
-		fail("Not yet implemented");
+		Action monAction = Action.newBuildSimpleAction(1,1, Tile.parseTile("Tile_FFFFZZ060123"));
+		assertTrue(monAction.positionTile1.equals(new Point(1,1)));
+		assertTrue(monAction.tile1.equals(Tile.parseTile("Tile_FFFFZZ060123")));
 	}
 
 	@Test
 	public void testNewBuildDoubleAction() {
-		fail("Not yet implemented");
+		Action monAction =Action.newBuildDoubleAction(new Point(0,1), Tile.parseTile("Tile_FFFFZZ060123"), new Point(2,3), Tile.parseTile("Tile_FFFFZZ060123"));
+		assertTrue(monAction.positionTile1.equals(new Point(0,1)));
+		assertTrue(monAction.tile1.equals(Tile.parseTile("Tile_FFFFZZ060123")));
+		assertTrue(monAction.positionTile2.equals(new Point(2,3)));
+		assertTrue(monAction.tile2.equals(Tile.parseTile("Tile_FFFFZZ060123")));
 	}
 
 	@Test
 	public void testGetClone() {
-		fail("Not yet implemented");
+		Action monActionSrc = Action.newStartTripNextTurnAction();
+		Action monActionCible = null;
+		monActionCible = monActionSrc.getClone();
+		assertFalse(monActionCible==monActionSrc);
+		assertTrue(monActionCible.action==monActionSrc.action);
+		
+		Point[] chemin = new Point[10];
+		for(int i=0; i<chemin.length;i++){
+			chemin[i] = new Point(i,i);
+		}
+		monActionSrc = Action.newMoveAction(chemin);
+		monActionCible = null;
+		monActionCible = monActionSrc.getClone();
+		assertFalse(monActionCible==monActionSrc);
+		assertTrue(monActionCible.action==monActionSrc.action);
+		for(int i=0; i<chemin.length; i++){
+			assertTrue(monActionCible.tramwayMovement[i].equals(monActionSrc.tramwayMovement[i]));
+		}
+
+		//TODO Ulysse: j'en suis ici 02/06/15 1h39
+		
 	}
 
 	@Test
