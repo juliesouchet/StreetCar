00093244ac899e9958103194ea1c3954feeba79f
sellaml <sellaml@f203-10.e-im2ag.ujf-grenoble.fr>
2015-06-01 16:57:07 +0200

    work in progress

--- src/main/java/gui/application/PlayerPanel.java
+++ src/main/java/gui/application/PlayerPanel.java
@@ -3,32 +3,61 @@ package main.java.gui.application;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 
+import main.java.data.Data;
+import main.java.data.Tile;
+import main.java.gui.board.TileImage;
 import main.java.gui.components.Panel;
+import main.java.player.PlayerIHM;
 
 public class PlayerPanel extends Panel{
 	
 	private static final long serialVersionUID = 1L;
 	
-	public PlayerPanel(String nameOfPlayer, String difficultyLevel) {		
+	String nameOfPlayer = null;
+	Data data;
+	Color playerColor;
+	
+	public PlayerPanel(String nameOfPlayer) {		
 		this.setBackground(Color.WHITE);
 		this.setPreferredSize(new Dimension(285, 175));
 		this.setLayout(null);
+		this.nameOfPlayer = nameOfPlayer;
 		JLabel nameOfPlayerLabel = new JLabel(nameOfPlayer);
-		JLabel difficultyLevelLabel = new JLabel(difficultyLevel);
+		//JLabel difficultyLevelLabel = new JLabel(difficultyLevel);
 
 		nameOfPlayerLabel.setBounds(70, 5, 80, 30);
-		difficultyLevelLabel.setBounds(70, 25, 80, 30);
+		//difficultyLevelLabel.setBounds(70, 25, 80, 30);
 		
 		this.add(nameOfPlayerLabel);
-		this.add(difficultyLevelLabel);		
+		//this.add(difficultyLevelLabel);
+		
+		//playerColor = data.getColor(); //TODO
+		
+		//
+	}
+	
+	public void setPlayerHandCards(String playerName) {
+		ArrayList<Tile> playerHand = new ArrayList<Tile>();
+		for (int i=0; i<data.getHandSize(playerName); i++){
+			
+			playerHand.add(data.getHandTile(playerName, i));
+			TileImage tileImage = new TileImage(playerHand.get(i));
+			tileImage.getImage();
+		}
 	}
 	
 	protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(10, 5, 45, 45); //avatar
+        
+        //avatar
+        g.setColor(playerColor);
+        g.fillRect(10, 5, 45, 45);
+        g.setColor(Color.BLACK);
+        g.drawRect(10, 5, 45, 45);
         
         int sizeOfCard = 45;
         int spaceBetween = 10;
@@ -51,4 +80,11 @@ public class PlayerPanel extends Panel{
         g.drawRect(10, 115, 45, 45); //station1
         g.drawRect(65, 115, 45, 45); //station2
     }
+
+	// Refresh game
+	
+	public void refreshGame(PlayerIHM player, Data data) {
+		this.data = data;
+		setPlayerHandCards(this.nameOfPlayer);
+	}
 }
