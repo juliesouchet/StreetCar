efd9f550a28479f8acddbcd99912fb6f5554060e
Jeremy Vizzini <contact@jeremyvizzini.com>
2015-06-01 15:08:44 +0200

    Fix map panel drawings

--- src/main/java/gui/board/TileImage.java
+++ src/main/java/gui/board/TileImage.java
@@ -1,39 +1,55 @@
 package main.java.gui.board;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.Hashtable;
 
+import main.java.data.Tile;
 import main.java.gui.util.Resources;
 import main.java.util.Direction;
 
 public class TileImage {
 
+	// Static
+	
+	static private Hashtable<String, BufferedImage> tilesHashTable = null;
+	
+	static {
+		tilesHashTable = new Hashtable<String, BufferedImage>();
+	}
+	
 	// Properties
 	
-	static Hashtable<String, BufferedImage> tilesHashTable = new Hashtable<String, BufferedImage>();
-	String tileID;
-	BufferedImage bufferedImage;
-	Direction rotation;
+	private String tileID;
+	private BufferedImage tileImage;
+	private Direction rotation;
 	
 	// Constructors
 	
-	public TileImage() {
-		
+	public TileImage(Tile tile) {
+		this.tileID = tile.getTileID();
+		this.rotation = tile.getTileDirection();
 	}
 	
-	// Actions
+	// Getters
 	
-	public BufferedImage getImage(String tileID) {		
-		if (bufferedImage != null) {
-			return bufferedImage;
-		} else {
-			this.bufferedImage = Resources.imageNamed(tileID);
-			if (bufferedImage != null) { 
-				tilesHashTable.put(tileID, bufferedImage);
-				return bufferedImage;
-			}
+	public BufferedImage getImage() {		
+		if (this.tileImage != null) {
+			return this.tileImage;
 		}
-		return bufferedImage;
+
+		this.tileImage = tilesHashTable.get(this.tileID);
+		if (this.tileImage != null) {
+			return this.tileImage;
+		}
+		
+		this.tileImage = Resources.imageNamed(this.tileID);
+		if (this.tileImage != null) { 
+			tilesHashTable.put(this.tileID, this.tileImage);
+			return this.tileImage;
+		}
+		return this.tileImage;
 	}	
 	
 	public Direction getRotation() {
@@ -44,5 +60,18 @@ public class TileImage {
 		return this.tileID;
 	}
 	
+	// Drawings
+	
+	public void drawInGraphics(Graphics2D g2d, int x, int y, int width) {
+		BufferedImage image = this.getImage();
+		
+		AffineTransform at = new AffineTransform();
+		at.translate(x, y);
+		at.rotate(Math.toRadians(this.getRotation().getVal()*90));
+		at.scale((double)width / (double)image.getWidth(),
+				 (double)width / (double)image.getHeight());
+		
+		g2d.drawImage(image, at, null);
+	}
+	
 }
-
