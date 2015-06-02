d2c87219e86a4045b93d477673f6818987fdf85c
UlysseCoutaud <luly_30@hotmail.fr>
2015-06-02 18:43:51 +0200

    Erreur repéré dans parseTile (Tile_FFFFZZ2003 isempty()==true) + test unitaire continus

--- src/test/java/game/TestUnitaireTile.java
+++ src/test/java/game/TestUnitaireTile.java
@@ -34,12 +34,141 @@ public class TestUnitaireTile {
 
 	@Test
 	public void testGetClone() {
-		fail("Not yet implemented");
+		Tile maTileA = Tile.parseTile("Tile_FFFFZZ2113");
+		Tile maTileB = null;
+
+		maTileB = maTileA.getClone();
+		
+		assertTrue(maTileA.equalsStrong(maTileB));
+		maTileA = Tile.parseTile("Tile_TFFFZZ040213");
+		assertTrue(maTileB.equalsStrong(Tile.parseTile("Tile_FFFFZZ2113")));
+
 	}
 
 	@Test
 	public void testParseTile() {
-		fail("Not yet implemented");
+		Tile maTile1 = Tile.parseTile("Tile_FFFFZZ99");
+		assertTrue(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertFalse(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertFalse(maTile1.isPathTo(Direction.NORTH));
+		assertFalse(maTile1.isPathTo(Direction.EAST));
+		assertFalse(maTile1.isPathTo(Direction.SOUTH));
+		assertFalse(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));
+		
+		Tile maTile2 = Tile.parseTile("Tile_FFFFZZ2003");
+		assertFalse(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertTrue(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertFalse(maTile1.isPathTo(Direction.NORTH));
+		assertFalse(maTile1.isPathTo(Direction.EAST));
+		assertTrue(maTile1.isPathTo(Direction.SOUTH));
+		assertTrue(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));
+		
+		Tile maTile3 = Tile.parseTile("Tile_FFFFZZ2113");
+		assertFalse(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertTrue(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertTrue(maTile1.isPathTo(Direction.NORTH));
+		assertFalse(maTile1.isPathTo(Direction.EAST));
+		assertTrue(maTile1.isPathTo(Direction.SOUTH));
+		assertFalse(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));
+		
+		Tile maTile4 = Tile.parseTile("Tile_FFFFZZ060123");
+		assertFalse(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertTrue(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertTrue(maTile1.isPathTo(Direction.NORTH));
+		assertTrue(maTile1.isPathTo(Direction.EAST));
+		assertTrue(maTile1.isPathTo(Direction.SOUTH));
+		assertTrue(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));
+		
+		Tile maTile5 = Tile.parseTile("Tile_FFFFZZ100102");
+		assertFalse(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertTrue(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertTrue(maTile1.isPathTo(Direction.NORTH));
+		assertTrue(maTile1.isPathTo(Direction.EAST));
+		assertFalse(maTile1.isPathTo(Direction.SOUTH));
+		assertTrue(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));		
+		
+		Tile maTile6 = Tile.parseTile("Tile_FFFFZZ100103");
+		assertFalse(maTile1.isEmpty());
+		assertFalse(maTile1.isBuilding());
+		assertTrue(maTile1.isDeckTile());
+		assertFalse(maTile1.isStop());
+		assertFalse(maTile1.isTerminus());
+		assertFalse(maTile1.isTree());
+		assertTrue(maTile1.isPathTo(Direction.NORTH));
+		assertTrue(maTile1.isPathTo(Direction.EAST));
+		assertFalse(maTile1.isPathTo(Direction.SOUTH));
+		assertTrue(maTile1.isPathTo(Direction.WEST));
+		assertTrue(maTile1.getTileDirection().equals(Direction.WEST));			
+		
+		Tile maTile7 = Tile.parseTile("Tile_FFFFZZ100203");
+		Tile maTile8 = Tile.parseTile("Tile_FFFTZ10101");
+		Tile maTile9 = Tile.parseTile("Tile_FFFTZ10103");
+		Tile maTile10 = Tile.parseTile("Tile_FFFTZ10112");
+		Tile maTile11 = Tile.parseTile("Tile_FFFTZ10123");
+		Tile maTile12 = Tile.parseTile("Tile_FFFTZ20101");
+		Tile maTile13 = Tile.parseTile("Tile_FFFTZ20103");
+		Tile maTile14 = Tile.parseTile("Tile_FFFTZ20112");
+		Tile maTile15 = Tile.parseTile("Tile_FFFTZ20123");
+		Tile maTile16 = Tile.parseTile("Tile_FFFTZ30101");
+		Tile maTile17 = Tile.parseTile("Tile_FFFTZ30103");
+		Tile maTile18 = Tile.parseTile("Tile_FFFTZ30112");
+		Tile maTile19 = Tile.parseTile("Tile_FFFTZ30123");
+		Tile maTile20 = Tile.parseTile("Tile_FFFTZ40101");
+		Tile maTile21 = Tile.parseTile("Tile_FFFTZ40103");
+		Tile maTile22 = Tile.parseTile("Tile_FFFTZ40112");
+		Tile maTile23 = Tile.parseTile("Tile_FFFTZ40123");
+		Tile maTile24 = Tile.parseTile("Tile_FFFTZ50101");
+		Tile maTile25 = Tile.parseTile("Tile_FFFTZ50103");
+		Tile maTile26 = Tile.parseTile("Tile_FFFTZ50112");
+		Tile maTile27 = Tile.parseTile("Tile_FFFTZ50123");
+		Tile maTile28 = Tile.parseTile("Tile_FFFTZ60101");
+		Tile maTile29 = Tile.parseTile("Tile_FFFTZ60103");
+		Tile maTile30 = Tile.parseTile("Tile_FFFTZ60112");
+		Tile maTile31 = Tile.parseTile("Tile_FFFTZ60123");
+		Tile maTile32 = Tile.parseTile("Tile_FTFFAZ01");
+		Tile maTile33 = Tile.parseTile("Tile_FTFFBZ01");
+		Tile maTile34 = Tile.parseTile("Tile_FTFFCZ01");
+		Tile maTile35 = Tile.parseTile("Tile_FTFFDZ01");
+		Tile maTile36 = Tile.parseTile("Tile_FTFFEZ01");
+		Tile maTile37 = Tile.parseTile("Tile_FTFFFZ01");
+		Tile maTile38 = Tile.parseTile("Tile_FTFFGZ01");
+		Tile maTile39 = Tile.parseTile("Tile_FTFFHZ01");
+		Tile maTile40 = Tile.parseTile("Tile_FTFFIZ01");
+		Tile maTile41 = Tile.parseTile("Tile_FTFFKZ01");
+		Tile maTile42 = Tile.parseTile("Tile_FTFFLZ01");
+		Tile maTile43 = Tile.parseTile("Tile_FTFFMZ01");
+		Tile maTile44 = Tile.parseTile("Tile_TFFFZZ040213");
+		Tile maTile45 = Tile.parseTile("Tile_TFFFZZ02010223");
+		Tile maTile46 = Tile.parseTile("Tile_TFFFZZ02021203");
+		Tile maTile47 = Tile.parseTile("Tile_TFFFZZ06031323");
+		Tile maTile48 = Tile.parseTile("Tile_TFFFZZ06121323");
+		Tile maTile49 = Tile.parseTile("Tile_TFFFZZ0401122303");
+
 	}
 
 	@Test
