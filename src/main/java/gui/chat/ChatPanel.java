995cbc8b775c81309d22f771b9571330fba77520
Jeremy Vizzini <contact@jeremyvizzini.com>
2015-06-02 12:22:58 +0200

    Add buffer to chat panel (work in progress)

--- src/main/java/gui/chat/ChatPanel.java
+++ src/main/java/gui/chat/ChatPanel.java
@@ -1,27 +1,14 @@
 package main.java.gui.chat;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.font.FontRenderContext;
-import java.awt.font.LineBreakMeasurer;
-import java.awt.font.TextAttribute;
-import java.awt.font.TextLayout;
-import java.awt.geom.GeneralPath;
-import java.awt.image.BufferedImage;
-import java.text.AttributedCharacterIterator;
-import java.text.AttributedString;
-import java.util.Hashtable;
-
 import main.java.gui.components.Panel;
 
 @SuppressWarnings("serial")
 public class ChatPanel extends Panel {
-	
+	/*
 	// Static 
 	
 	private static Hashtable<TextAttribute, Object> TEXT_ATTRIBUTES = null;
+	private static FontRenderContext FONT_RENDER = null;
 	private static int AVATAR_SIZE = 30;
 	private static int TEXT_MARGIN_X = 10;
 	private static int TEXT_MARGIN_Y = 5;
@@ -33,6 +20,7 @@ public class ChatPanel extends Panel {
     	TEXT_ATTRIBUTES.put(TextAttribute.FAMILY, "Serif");
     	TEXT_ATTRIBUTES.put(TextAttribute.SIZE, new Float(18.0));
     	TEXT_ATTRIBUTES.put(TextAttribute.FOREGROUND, Color.BLACK);
+    	FONT_RENDER = new  FontRenderContext(new AffineTransform(), true, true);
     }
 	
 	// Properties
@@ -54,6 +42,7 @@ public class ChatPanel extends Panel {
 		this.chat.addMessage(null, "Test3", "Nunc accumsan tincidunt dui eget hendrerit. Sed eget lacus velit. Donec laoreet venenatis suscipit. Morbi gravida justo nibh, at vestibulum nisl blandit et. Fusce posuere pellentesque nulla nec luctus. Nullam molestie condimentum dui, quis venenatis leo iaculis vel.");
 		this.chat.addMessage(null, "Test4", "Nullam a orci nec enim suscipit consequat. Nunc a malesuada neque, sit amet facilisis est. Maecenas rhoncus lobortis libero sed posuere. ");
 		this.sender = "Test3";
+		this.reloadBuffer();
 	}
 	
 	// Getters
@@ -70,19 +59,27 @@ public class ChatPanel extends Panel {
 	
 	public void setChat(Chat chat) {
 		this.chat = chat;
-		this.revalidate();
 	}
 	
 	public void setSender(String sender) {
 		this.sender = sender;
-		this.revalidate();
 	}
 	
 	// Buffers
 	
 	public void reloadBuffer() {
+		if (this.chat == null || this.sender == null) {
+			return;
+		}
+        
 		int height = 1;
 		int width = this.getWidth();
+		for (int i = 0; i < this.chat.getNumberOfMessages(); i++) {
+			String text = chat.getMessageAtIndex(i).getText();
+	        AttributedString attrText = new AttributedString(text, TEXT_ATTRIBUTES);
+			Rectangle messageRect = this.computeMessageRect(attrText); 
+			height += messageRect.height;
+		}
 		
 		this.buffer = new BufferedImage(height, width, BufferedImage.TYPE_INT_RGB);
 		this.graphics = this.buffer.createGraphics();
@@ -90,7 +87,24 @@ public class ChatPanel extends Panel {
         this.graphics.setPaint(Color.white);
         this.graphics.fillRect(0, 0, width, height);
 		
-        
+        int originY = 0;
+		for (int i = 0; i < chat.getNumberOfMessages() ; i++) {
+			ChatMessage message = this.chat.getMessageAtIndex(i);
+			String text = message.getText();
+			
+	        AttributedString attrText = new AttributedString(text, TEXT_ATTRIBUTES);
+	        BufferedImage image = message.getAvatarImage();
+	        Color color = Color.RED;
+	        boolean isSender = message.getSender().equals(this.sender);
+	        
+	        Rectangle rect = this.computeMessageRect(attrText);
+	        rect.y += originY;
+	        originY += rect.height;
+	        
+	        this.drawBubble(color, rect, isSender);
+	        this.drawText(attrText, rect, isSender);
+	        this.drawAvatar(image, rect, isSender);
+		}
 	}
 	
 	public void updateBuffer() {
@@ -103,35 +117,19 @@ public class ChatPanel extends Panel {
     public void paintComponent(Graphics g) {
     	super.paintComponent(g);
     	
-        if (this.chat == null)  return;
-
-        Graphics2D g2d = (Graphics2D)g;
-        int originY = 0;
-		for (int i = 0; i < chat.getNumberOfMessages() ; i++) {
-			ChatMessage message = this.chat.getMessageAtIndex(i);
-			String text = message.getText();
-			
-	        AttributedString attrText = new AttributedString(text, TEXT_ATTRIBUTES);
-	        BufferedImage image = message.getAvatarImage();
-	        Color color = Color.RED;
-	        boolean isSender = message.getSender().equals(this.sender);
-	        
-	        Rectangle rect = this.computeMessageRect(g2d, attrText);
-	        rect.y += originY;
-	        originY += rect.height;
-	        
-	        this.drawBubble(g2d, color, rect, isSender);
-	        this.drawText(g2d, attrText, rect, isSender);
-	        this.drawAvatar(g2d, image, rect, isSender);
-		}
+    	if (this.buffer == null) {
+    		return;
+    	}
+    	
+    	Graphics2D g2d = (Graphics2D)g;
+    	g2d.drawImage(this.buffer, 0, 0, null);
     }
     
-    private Rectangle computeMessageRect(Graphics2D g2d, AttributedString attrText) {
+    private Rectangle computeMessageRect(AttributedString attrText) {
     	AttributedCharacterIterator paragraph = attrText.getIterator();
     	int paragraphStart = paragraph.getBeginIndex();
     	int paragraphEnd = paragraph.getEndIndex();
-    	FontRenderContext frc = g2d.getFontRenderContext();
-    	LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, frc);
+    	LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, FONT_RENDER);
     	
         float drawPosY = 0;
         float width = this.getWidth() - AVATAR_SIZE - 2 * (BUBBLE_MARGIN_X + TEXT_MARGIN_X);
@@ -148,7 +146,8 @@ public class ChatPanel extends Panel {
         return rect;
     }
     
-    public void drawAvatar(Graphics2D g2d, BufferedImage image, Rectangle rect, Boolean isSender) {
+    public void drawAvatar(BufferedImage image, Rectangle rect, Boolean isSender) {
+    	Graphics2D g2d = this.graphics;
     	if (isSender) {
     		g2d.setColor(Color.BLUE);
     		g2d.fillRect(rect.x + rect.width - AVATAR_SIZE - BUBBLE_MARGIN_X,
@@ -164,7 +163,8 @@ public class ChatPanel extends Panel {
     	}
     }
     
-    public void drawBubble(Graphics2D g2d, Color color, Rectangle rect, boolean isSender) {
+    public void drawBubble(Color color, Rectangle rect, boolean isSender) {
+    	Graphics2D g2d = this.graphics;
     	GeneralPath path =  new GeneralPath(GeneralPath.WIND_EVEN_ODD);
     	int x, y;
     	if (isSender) {
@@ -195,12 +195,11 @@ public class ChatPanel extends Panel {
     	g2d.fill(path);
     }
     
-    public void drawText(Graphics2D g2d, AttributedString text, Rectangle rect, boolean isSender) {
+    public void drawText(AttributedString text, Rectangle rect, boolean isSender) {
     	AttributedCharacterIterator paragraph = text.getIterator();
     	int paragraphStart = paragraph.getBeginIndex();
     	int paragraphEnd = paragraph.getEndIndex();
-    	FontRenderContext frc = g2d.getFontRenderContext();
-    	LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, frc);
+    	LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(paragraph, FONT_RENDER);
     	
     	int x, y, width;
     	if (isSender) {
@@ -217,9 +216,9 @@ public class ChatPanel extends Panel {
         	TextLayout layout = lineMeasurer.nextLayout(width);
             float drawPosX = layout.isLeftToRight() ? 0 : width - layout.getAdvance();
             drawPosY += layout.getAscent();
-            layout.draw(g2d, x + drawPosX, y + drawPosY);
+            layout.draw(this.graphics, x + drawPosX, y + drawPosY);
             drawPosY += layout.getDescent() + layout.getLeading();
         }
-    }
+    }*/
     
 }
