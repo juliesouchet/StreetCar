362b2cab81ff89053fcf35f0c8834168b6985c78
Julie Souchet <souchet0julie@gmail.com>
2015-06-02 14:11:07 +0200

    Fusion

--- src/main/java/automaton/test/TestAIvsAI.java
+++ src/main/java/automaton/test/TestAIvsAI.java
@@ -3,6 +3,9 @@ package main.java.automaton.test;
 import java.awt.Color;
 import java.rmi.RemoteException;
 
+import main.java.data.LoginInfo;
+import main.java.game.ExceptionForbiddenAction;
+import main.java.game.ExceptionForbiddenHostModification;
 import main.java.game.ExceptionGameHasAlreadyStarted;
 import main.java.game.ExceptionUnknownBoardName;
 import main.java.game.Game;
@@ -30,7 +33,7 @@ public class TestAIvsAI {
 		}	
 		name1 = level1==1 ? dumbestName : travelerName;
 		name1 += " 1";
-		name2 = level1==1 ? dumbestName : travelerName;
+		name2 = level2==1 ? dumbestName : travelerName;
 		name2 += " 2";
 		
 		try {
@@ -39,9 +42,23 @@ public class TestAIvsAI {
 			System.out.println("Game creation error"); e.printStackTrace();
 		}
 
+		// TODO virer les joueurs créés initialement
+		LoginInfo hostLogin = LoginInfo.getInitialLoggedPlayerTableCell(0);
+		String hostName = hostLogin.getPlayerName();
+		for (int i = 4; i >= 0; i--) {
+			try {
+				game.setLoginInfo(hostName, i, new LoginInfo(true, null, false, true, 0));
+			} catch (RemoteException | ExceptionForbiddenAction
+					| ExceptionForbiddenHostModification e) {
+				e.printStackTrace();
+			}
+		}
+		
 		TestDumbestIHM ihm = new TestDumbestIHM();
 		try {
+	System.out.println("Create player 1");
 			player1 = new PlayerAI(name1, true, game, level1, ihm);
+	System.out.println("Create player 2");
 			player2 = new PlayerAI(name2, false, game, level2, ihm);
 			player1.setPlayerColor(Color.red);
 			player2.setPlayerColor(Color.green);
