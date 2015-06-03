6b5e8ea2d04e2e8d6c1e726f6359c66420a609de
Valysaar <valpruliere@gmail.com>
2015-06-03 16:54:51 +0000

    useless exception

--- src/main/java/gui/menus/HostRoomMenuPanel.java
+++ src/main/java/gui/menus/HostRoomMenuPanel.java
@@ -14,7 +14,6 @@ import main.java.data.Data;
 import main.java.data.LoginInfo;
 import main.java.game.ExceptionForbiddenAction;
 import main.java.game.ExceptionForbiddenHostModification;
-import main.java.game.ExceptionNonInitializedPlayer;
 import main.java.game.ExceptionNotEnougthPlayers;
 import main.java.gui.application.GameController;
 import main.java.gui.application.StreetCar;
@@ -115,8 +114,6 @@ public class HostRoomMenuPanel extends MenuPanel {
 		} catch (ExceptionForbiddenAction e) {
 			e.printStackTrace();
 		} catch (ExceptionNotEnougthPlayers e) {
-			e.printStackTrace();
-		} catch (ExceptionNonInitializedPlayer e) {
 			e.printStackTrace();
 		}
 	}
