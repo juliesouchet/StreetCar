0f4c4e979643236f372efef8c2e6c04bf6c72f7c
Julie Souchet <souchet0julie@gmail.com>
2015-06-03 10:53:21 +0200

    Ajout doc aux méthodes

--- src/main/java/game/Engine.java
+++ src/main/java/game/Engine.java
@@ -129,7 +129,7 @@ public class Engine implements Runnable
 	{
 		PlayerInterface pi;
 
-		if ((data.isGameStarted()) || (data.istHost(playerName)))
+		if ((data.isGameStarted()) || (data.isHost(playerName)))
 		{
 			for (String name: data.getPlayerNameList()) data.getRemotePlayer(name).excludePlayer();
 			this.engineThread.interrupt();
