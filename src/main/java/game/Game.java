0f2ea1fb48f71c4d2515616134d0fd67f7cb93f4
Valysaar <valpruliere@gmail.com>
2015-06-05 20:57:05 +0000

    called Thread.currentThread().interrupt(); instead of system.exit(0) when game finishes or when host leaves game

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -229,14 +229,17 @@ System.out.println("Game.setLoginInfo: no change to do");
 
 		this.loggedPlayerTable[playerIndex] = LoginInfo.getInitialLoggedPlayerTableCell(playerIndex);
 		this.engine.addAction(data, "excludePlayer", data.getRemotePlayer(playerName));
-		this.engine.onQuitGame(this.data, playerName);
+//TODO this.engine.onQuitGame(this.data, playerName);
 		System.out.println("\n===========================================================");
 		System.out.println(gameMessageHeader + "quitGame");
 		System.out.println(gameMessageHeader + "logout result : player logged out");
 		System.out.println(gameMessageHeader + "playerName    : " + playerName);
 		System.out.println("===========================================================\n");
 
-		if (gameHasStarted || isHost)	Thread.currentThread().isInterrupted();
+		if (gameHasStarted || isHost)	
+		{
+			Thread.currentThread().interrupt();
+		}
 	}
 
 	/**==============================================
